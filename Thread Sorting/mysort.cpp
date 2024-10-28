1bf9596f5039b5196d78a2cf9aefa9b6cd614209
Msithiphong <Csithiphonggg@gmail.com>
2024-10-28 03:48:44 -0700

    Threads

--- Thread Sorting/mysort.cpp
+++ Thread Sorting/mysort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <fstream>
+#include <mutex>
 
 using namespace std;
 
@@ -32,25 +33,43 @@ int main(int argc, char* argv[])
         }
 
         int sortNum = count / 2;
+        int sectionNums = count / 16;
        
-        thread t1(bubble, a, sortNum);
-        thread t2(bubble, a, sortNum);
-        thread t3(bubble, a, sortNum);
-        thread t4(bubble, a, sortNum);
-        thread t5(bubble, a, sortNum);
-        thread t6(bubble, a, sortNum);
-        thread t7(bubble, a, sortNum);
-        thread t8(bubble, a, sortNum);
-        thread t9(bubble, a, sortNum);
-        thread t10(bubble, a, sortNum);
-        thread t11(bubble, a, sortNum);
-        thread t12(bubble, a, sortNum);
-        thread t13(bubble, a, sortNum);
-        thread t14(bubble, a, sortNum);
-        thread t15(bubble, a, sortNum);
-        thread t16(bubble, a, sortNum);
+        thread t1(bubble, a[0 * sectionNums], a[1 * sectionNums] - 1);
+        thread t2(bubble, a[1 * sectionNums], a[2 * sectionNums] - 1);
+        thread t3(bubble, a[2 * sectionNums], a[3 * sectionNums] - 1);
+        thread t4(bubble, a[3 * sectionNums], a[4 * sectionNums] - 1);
+        thread t5(bubble, a[4 * sectionNums], a[5 * sectionNums] - 1);
+        thread t6(bubble, a[5 * sectionNums], a[6 * sectionNums] - 1);
+        thread t7(bubble, a[6 * sectionNums], a[7 * sectionNums] - 1);
+        thread t8(bubble, a[7 * sectionNums], a[8 * sectionNums] - 1);
+        thread t9(bubble, a[8 * sectionNums], a[9 * sectionNums] - 1);
+        thread t10(bubble, a[9 * sectionNums], a[10 * sectionNums] - 1);
+        thread t11(bubble, a[10 * sectionNums], a[11 * sectionNums] - 1);
+        thread t12(bubble, a[11 * sectionNums], a[12 * sectionNums] - 1);
+        thread t13(bubble, a[12 * sectionNums], a[13 * sectionNums] - 1);
+        thread t14(bubble, a[13 * sectionNums], a[14 * sectionNums] - 1);
+        thread t15(bubble, a[14 * sectionNums], a[15 * sectionNums] - 1);
+        thread t16(bubble, a[15 * sectionNums], a[count] - 1);
 
         
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        t6.join();
+        t7.join();
+        t8.join();
+        t9.join();
+        t10.join();
+        t11.join();
+        t12.join();
+        t13.join();
+        t14.join();
+        t15.join();
+        t16.join();
+
         for (int i = 0; i < count; i++){
             fout << a[i] << endl;
             
@@ -67,11 +86,16 @@ int main(int argc, char* argv[])
 
 void bubble(int A[], int size)
     {
+        mutex mtx;
+        mtx.lock();
         for (int i = 0; i < size - 1; i++)
             for (int j = 0; j < size - i - 1; j++)
             {
                 if (A[j] > A[j + 1])
                     swap(A[j], A[j + 1]);
             }
+        mtx.unlock();
     }
 
+
+void mergeAdjacent()
